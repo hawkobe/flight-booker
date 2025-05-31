4ade2ee69c0d73bafc0c33fd29e5ed8feaff8f99
Jacob Sanders <sanders.jms@gmail.com>
2025-05-30 21:54:49 -0600

    created models for booking and passenger and associated both with flights

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
   belongs_to :departure_airport, class_name: "Airport"
 
+  has_many :bookings
+  has_many :passengers, through: :bookings
+
   def formatted_date
     departure_time.strftime("%B %d, %Y")
   end
